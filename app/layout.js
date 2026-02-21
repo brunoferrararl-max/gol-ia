73987b539f372f6b9b6bc8520227c08153e1b81d
brunoferrararl-max <brunoferrararl@gmail.com>
2026-02-21 02:40:33 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Gol IA",
+  description: "App de análise de apostas",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
